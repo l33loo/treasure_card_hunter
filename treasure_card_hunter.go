940261745e9d92ca063216ef8c3c6edb97d9008a
Lila Karpowicz <lila@karpowicz.dev>
2023-10-30 10:24:27 -0100

    compelte main functionalities

--- treasure_card_hunter.go
+++ treasure_card_hunter.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -43,17 +44,36 @@ intro:
 
 	// single player mode
 	if gameModeChoice == "1" {
-		for {
-			//
+		points := 0
+		tries := 0
+		for points < 3 {
+			printMap(gameMap)
+			fmt.Println("Pick a place on the map.\nEnter the coordinates as x, y, like 2, 3")
 
+			mapCoord, _ := reader.ReadString('\n')
+			sp := strings.Split(mapCoord, ",")
+			coordX, _ := strconv.Atoi(strings.TrimSpace(sp[0]))
+			// if error || if out of range
+			coordY, _ := strconv.Atoi(strings.TrimSpace(sp[1]))
+			// if error || if out of range
+			foundTreasure := updateMap(&gameMap, coordX-1, coordY-1)
+			printMap(gameMap)
+			if foundTreasure {
+				points++
+			}
+			tries++
 			break
 		}
+		str := fmt.Sprintf("Treasures all found with %d tries!", tries)
+		fmt.Println(str)
 		// multiplayer mode
 	} else {
 		cards := []int{2, 3, 4, 5, 6, 7, 8, 9, 10}
+		player1Points := 0
+		player2Points := 0
 
-		// full fame
-		for {
+		// full game
+		for player1Points+player2Points < 3 {
 			// card round
 			for {
 				player1Card := cards[rand.Intn(len(cards))]
@@ -64,7 +84,9 @@ intro:
 
 				if player1Card > player2Card {
 					roundWinner = 1
-				} else if player1Card < player2Card {
+				}
+
+				if player1Card < player2Card {
 					roundWinner = 2
 				}
 
@@ -78,11 +100,36 @@ intro:
 				fmt.Println(str)
 
 				printMap(gameMap)
+				str2 := fmt.Sprintf("Player %d, pick a place on the map.\nEnter the coordinates as x, y, like 2, 3", roundWinner)
+				fmt.Println(str2)
+
+				mapCoord, _ := reader.ReadString('\n')
+				sp := strings.Split(mapCoord, ",")
+				coordX, _ := strconv.Atoi(strings.TrimSpace(sp[0]))
+				// if error || if out of range
+				coordY, _ := strconv.Atoi(strings.TrimSpace(sp[1]))
+				// if error || if out of range
+				foundTreasure := updateMap(&gameMap, coordX-1, coordY-1)
+				printMap(gameMap)
+				if foundTreasure {
+					if roundWinner == 1 {
+						player1Points++
+					}
+
+					if roundWinner == 2 {
+						player2Points++
+					}
+				}
 
 				break
 			}
-			//
-			break
+			if player1Points > player2Points {
+				str := fmt.Sprintf("Player 1 wins with {%d} points!", player1Points)
+				fmt.Println(str)
+			} else {
+				str := fmt.Sprintf("Player 2 wins with {%d} points!", player2Points)
+				fmt.Println(str)
+			}
 		}
 	}
 }
@@ -94,25 +141,30 @@ func createMap(countTreasures int) [6]Row {
 	// fmt.Printf("%v", gameMap)
 
 	for i := 0; i < countTreasures; i++ {
-		x := rand.Intn(countTreasures)
-		y := rand.Intn(countTreasures)
+		x := rand.Intn(6)
+		y := rand.Intn(6)
 
+		for gameMap[x][y] == "T" {
+			x = rand.Intn(6)
+			y = rand.Intn(6)
+		}
 		gameMap[x][y] = "T"
 	}
 
 	return gameMap
 }
 
-func updateMap(gameMap [6]Row, x int, y int) bool {
-	cell := gameMap[x][y]
+func updateMap(gameMap *[6]Row, x int, y int) bool {
+	cell := (*gameMap)[x][y]
 	if cell == "T" {
-		gameMap[x][y] = "X"
+		(*gameMap)[x][y] = "X"
 		return true
 	} else if cell == "" {
-		gameMap[x][y] = "O"
+		(*gameMap)[x][y] = "O"
 		return false
 	} else {
 		// already guessed
+		fmt.Println("Already picked")
 		return false
 	}
 }
